3dba73190e7ff6e9eba05a04c2d80cbfe5db3f0e
qf6672wr@go.minnstate.edu <qf6672wr@go.minnstate.edu>
2022-08-31 19:46:28 -0500

    elif else cont

--- .py
+++ .py
@@ -1,12 +1,6 @@
-temperature = 15
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 20:
-    print("It's nice")
-else:
-    print("It's cold")
-print("Done")
+age = 12
+message = "Eligible" if age >=18 else "Not eligible"
+print(message)
 
 
 
