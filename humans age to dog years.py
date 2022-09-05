4764bb530aa2e8b0b33cf81487ac4d65acc71598
qf6672wr@go.minnstate.edu <qf6672wr@go.minnstate.edu>
2022-09-05 14:54:42 -0500

    lab 2 part 1a

---
+++ humans age to dog years.py
@@ -0,0 +1,9 @@
+#Create a variable to store humans age 
+#Calculate how old the human is in dog years
+#asking user for age/converting input str to int
+#Create a script that asks the user for name, lastname, and age. Print out the info as follows.
+
+humans_age = int(input("How old are you?"))
+dog_years = 7
+human_age_in_dog_years = humans_age * dog_years
+print ("Your age in dog years is", human_age_in_dog_years) 
